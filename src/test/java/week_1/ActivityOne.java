149dc465c0081ce547e23a7e7aa421daa4b3cace
aseelr7 <aseelr95@gmail.com>
2024-10-24 19:32:34 -0400

    Thursday Oct24 Review Session Activities

--- src/test/java/week_1/ActivityOne.java
+++ src/test/java/week_1/ActivityOne.java
@@ -1,21 +1,34 @@
 package week_1;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.Scanner;
 
 public class ActivityOne {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("For opening the app with chrome press one \n" +
+                "for opening app with edge press two: ");
+        int userChoice = userInput.nextInt();
 
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://dev.retail.tekschool-students.com/");
-        System.out.println("Chrome Title: " + chromeDriver.getTitle());
+        if (userChoice == 1 ){
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://dev.retail.tekschool-students.com/");
+            System.out.println("Chrome Title: " + driver.getTitle());
 
-        chromeDriver.quit();
+            driver.quit();
+        } else if (userChoice == 2 ){
+            WebDriver driver = new EdgeDriver();
+            driver.get("https://dev.retail.tekschool-students.com/");
+            System.out.println("Edge Title: " + driver.getTitle());
+            driver.quit();
+        } else System.out.println("Invalid Choice!");
 
-        EdgeDriver edgeDriver = new EdgeDriver();
-        edgeDriver.get("https://dev.retail.tekschool-students.com/");
-        System.out.println("Edge Title: " + edgeDriver.getTitle());
-        edgeDriver.quit();
+
+
+
     }
 }
