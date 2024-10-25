7d91695ca4aacd0a0e9ff0e8746261a3259333fa
aseelr7 <aseelr95@gmail.com>
2024-10-24 20:07:24 -0400

    Thursday Oct24 Review Session Activity 2

---
+++ src/test/java/week_1/ActivityTwo.java
@@ -0,0 +1,45 @@
+package week_1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+public class ActivityTwo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://dev.retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        By searchBarLocator = By.className("ant-input");
+        WebElement searchBarElement = driver.findElement(searchBarLocator);
+        searchBarElement.click();
+        searchBarElement.sendKeys("Apple");
+
+        By searchButton = By.className("ant-btn");
+        WebElement searchButtonElement = driver.findElement(searchButton);
+        searchButtonElement.click();
+        Thread.sleep(500);
+
+        By cardsLocator = By.tagName("h4");
+        List<WebElement> cards = driver.findElements(cardsLocator);
+
+        int cardCounter = 0;
+        for (WebElement card : cards){
+            cardCounter++;
+        }
+
+        if (cardCounter == 3 ){
+            System.out.println("Test Passed");
+        } else System.out.println("Test Failed not 3 cards on the page");
+
+        driver.quit();
+
+    }
+
+}
