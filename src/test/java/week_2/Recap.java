0c55bd060a44a499e55d6fe49226330197f033cb
aseelr7 <aseelr95@gmail.com>
2024-10-28 19:15:40 -0400

    Week2 Day1 Recap Activity.

---
+++ src/test/java/week_2/Recap.java
@@ -0,0 +1,38 @@
+package week_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Recap {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+       WebElement searchBar = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[1]/div[1]/div[2]/div/input"));
+       searchBar.click();
+       searchBar.sendKeys("TV");
+
+       WebElement searchButton = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[1]/div[1]/div[2]/div/button"));
+       searchButton.click();
+
+       Thread.sleep(1000);
+
+        WebElement firstTv = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[3]/div/div[1]/p[1]"));
+        firstTv.click();
+
+        Thread.sleep(1000);
+
+        WebElement tvPage = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div[2]/h1"));
+        System.out.println(tvPage.getText());
+
+        driver.quit();
+
+
+    }
+}
