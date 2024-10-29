dc82d190990e60f5156bd6f23b30b7aca1e82851
aseelr7 <aseelr95@gmail.com>
2024-10-28 20:27:27 -0400

    Week2 Day1 Recap Activity.

---
+++ src/test/java/week_2/RelativeXpathActivity.java
@@ -0,0 +1,38 @@
+package week_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeXpathActivity {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+        WebElement searchBar = driver.findElement(By.xpath("//input[@id = 'searchInput']"));
+        searchBar.click();
+        searchBar.sendKeys("KeyBoard");
+
+        WebElement searchButton = driver.findElement(By.xpath("//button[@id = 'searchBtn' ]"));
+        searchButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement firstKeyboard = driver.findElement(By.xpath("//div[@class = 'products']/div[1]"));
+        firstKeyboard.click();
+
+        Thread.sleep(1000);
+
+        WebElement KeyPage = driver.findElement(By.xpath("//h1[@class = 'product__name']"));
+        System.out.println(KeyPage.getText());
+
+        driver.quit();
+
+
+    }
+}
