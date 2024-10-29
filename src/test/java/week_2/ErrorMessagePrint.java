97a89d3c935bf842d272c5e4d7bed73e40e9358a
aseelr7 <aseelr95@gmail.com>
2024-10-28 21:39:49 -0400

    Week2 Day1 Relative Xpath Fixed.

---
+++ src/test/java/week_2/ErrorMessagePrint.java
@@ -0,0 +1,34 @@
+package week_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class ErrorMessagePrint {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//a[@id = 'signinLink']")).click();
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//a[@id = 'newAccountBtn']")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//button[@id = 'signupBtn']")).click();
+        List< WebElement > errors = driver.findElements(By.xpath("//div[@class = 'error']"));
+        System.out.println( driver.findElement(By.xpath("//pre[@class= 'error']")).getText());
+
+        for (WebElement error : errors){
+            System.out.println(error.getText());
+        }
+
+        driver.quit();
+    }
+}
