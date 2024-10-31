eaf97c5157f0ae47426bd6d5916d99c0ce3a0b05
aseelr7 <aseelr95@gmail.com>
2024-10-30 21:37:45 -0400

    Week2 Day3 activity

--- src/test/java/week_2/Day_3/UpdateProfileWithWait.java
+++ src/test/java/week_2/Day_3/UpdateProfileWithWait.java
@@ -20,21 +20,20 @@ public class UpdateProfileWithWait {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 
+        wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//a[@id = 'signinLink']")))).click();
 
-        driver.findElement(By.xpath("//a[@id = 'signinLink']")).click();
-        driver.findElement(By.xpath("//input[@name = 'email']")).sendKeys("aseel@email.com");
-        driver.findElement(By.xpath("//input[@name = 'password']")).sendKeys("Aseel@123");
+        wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//input[@name = 'email']")))).sendKeys("aseel@email.com");
+        wait.until(ExpectedConditions.presenceOfElementLocated((By.xpath("//input[@name = 'password']")))).sendKeys("Aseel@123");
+
 
         WebElement signIn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type = 'submit']")));
         signIn.click();
 
-        driver.findElement(By.cssSelector("a#accountLink")).click();
+        wait.until(ExpectedConditions.elementToBeClickable((By.cssSelector("a#accountLink")))).click();
 
         //  driver.findElements(By.xpath("//div[@class = 'account__personal-grid']//input")).clear();
-
-
-        driver.findElement(By.cssSelector("input#personalPhoneInput")).clear();
-        driver.findElement(By.cssSelector("input#personalPhoneInput")).sendKeys("5133288897");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input#personalPhoneInput"))).clear();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input#personalPhoneInput"))).sendKeys("5113288897");
 
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button#personalUpdateBtn"))).click();
       WebElement toastify =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role = 'alert']/div[2]")));
