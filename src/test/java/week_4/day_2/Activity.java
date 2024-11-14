ff7dce341f21097fa4ea63851865c150c459d83e
aseelr7 <aseelr95@gmail.com>
2024-11-13 19:59:40 -0500

    Week4 day 2 Activity

--- src/test/java/week_4/day_2/Activity.java
+++ src/test/java/week_4/day_2/Activity.java
@@ -14,19 +14,23 @@ public class Activity {
         ElementsAndValidations ev = new ElementsAndValidations();
         ev.navigateToUrl(baseUrl);
 
+        // Clicking on Sign In Button
         ev.clickWithValidation("//a[@id = 'signinLink']");
+
+        //Sending the SignIn info
         ev.sendKeysWithValidation("//input[@id = 'email']",email);
         ev.sendKeysWithValidation("//input[@id = 'password']",password);
-
         ev.clickWithValidation("//button[@id = 'loginBtn']");
 
+        //Clicking on Account Tab
         ev.clickWithValidation("//a[@id = 'accountLink']");
 
+        //Updating the Info
         ev.sendKeysWithValidation("//input[@id = 'nameInput']",newName);
         ev.sendKeysWithValidation("//input[@id = 'emailInput']",newEmail);
-
         ev.clickWithValidation("//button[@id = 'personalUpdateBtn']");
 
+        // Verifying The New Info
         ev.textVerification("//h1[@class = 'account__information-username']",newName);
         ev.textVerification("//h1[@class = 'account__information-email']",newEmail);
 
